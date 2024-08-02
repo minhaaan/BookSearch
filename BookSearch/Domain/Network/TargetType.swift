36568fc83627caecfe4d6944bc80a29eb59f747a
최민한 <chmh0411@gmail.com>
2024-08-02 16:22:35 +0900

    add: TargetType

---
+++ BookSearch/Domain/Network/TargetType.swift
@@ -0,0 +1,13 @@
+//
+//  TargetType.swift
+//  BookSearch
+//
+//  Created by 최민한 on 8/2/24.
+//
+
+import Foundation
+
+protocol TargetType {
+  var baseURL: String { get }
+  var path: String { get }
+}
